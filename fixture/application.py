0c39142ba206a1e0f5bf7506f47b28d6d5a0d114
anna-inboxx <agness2B!>
2020-05-27 14:16:38 +0300

    протокол Telnet

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
+from fixture.james import JamesHelper
 
 
 class Application:
@@ -16,10 +17,9 @@ class Application:
         self.wd.implicitly_wait(2)
         self.session = SessionHelper(self)
         self.project = ProjectHelper(self)
+        self.james = JamesHelper(self)
         self.config = config
         self.base_url = config["web"]["baseUrl"]
-        self.username = config["webadmin"]["username"]
-        self.password = config["webadmin"]["password"]
 
     def is_valid(self):
         try:
